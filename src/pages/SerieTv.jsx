75d71a8ddb4c3a33ee73750f70d7802b4b3d8f6f
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-27 10:18:50 +0100

    creazione della ricerca per le serie tv

--- src/pages/SerieTv.jsx
+++ src/pages/SerieTv.jsx
@@ -1,6 +1,41 @@
+import { useState } from 'react'
+
 export default function SerieTv() {
+
+    const [searchTv, setSearchTv] = useState("")
+    const [tv,setTV] = useState([])
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        const API_KEY = import.meta.env.VITE_API_KEY;
+        const urlDue = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${searchTv}&language=it-IT`;
+
+
+    fetch(urlDue)
+            .then(res => res.json())
+            .then(data => {
+                setTV(data.results); 
+        });
+
+    }    
+        
     return(
         <>
+            <form role="search" onSubmit={handleSearch}>           
+                <input type="text" value={searchTv} onChange={(e) => setSearchTv(e.target.value)} placeholder="Cerca un serie tv..."/>
+                    <button type="submit">Cerca</button>
+                        <ul>
+                            {tv.map(tv => (
+                                <li key={tv.id}>
+                                    <h3>{tv.name}</h3>
+                                    <p>Titolo originale: {tv.original_title}</p>
+                                    <p>Lingua: {tv.original_language}</p>
+                                    <p>Voto: {tv.vote_average}</p>
+                                </li>
+                            ))}
+                        </ul>
+            </form>
         </>
     )
 }
