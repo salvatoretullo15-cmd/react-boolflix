40db0894636573761bc9844455a9e4e7199a0fab
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-30 00:32:35 +0200

    inizio milestone 4 iniziando dal logo di boolflix

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -5,17 +5,17 @@ export default function AppHeader() {
 
     return(
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-black">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <img className="navbar-brand" src="/boolflix-logo.png" alt="Logo"/>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <div className="navbar-nav me-auto mb-2 mb-lg-0">
                             {menu.map(item=> <NavLink className="nav-link" to={item.path} key={item.id} > {item.text}
                             </NavLink>)}
-                        </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
